814bea6b52de2cec485dd4859a90f30573b4093f
Junaid Jamel <109211380+JunaidJameel@users.noreply.github.com>
2025-11-29 10:44:07 +0500

    ClipRRect Widget

---
+++ lib/widgets/clip_r_rect_widget_06.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ClipRRectWidget extends StatelessWidget {
+  const ClipRRectWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(30),
+      child: Container(
+        // alignment: Alignment.topLeft,
+        height: 400,
+        width: 400,
+        decoration: BoxDecoration(
+          color: Colors.blue[200],
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Image.network(
+          'https://i.pinimg.com/1200x/fc/65/db/fc65db8ba66024d3c85f320bab36cc95.jpg',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
