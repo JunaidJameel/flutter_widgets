ae155c1d3389330e81942ce76865e86d76c50e92
Junaid Jamel <109211380+JunaidJameel@users.noreply.github.com>
2025-12-06 13:49:23 +0500

    AppBar Widget

--- lib/widgets/appBar_widget_14.dart
+++ lib/widgets/appBar_widget_14.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends StatelessWidget {
+  const AppBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 120,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.horizontal(
+          right: Radius.circular(40),
+          left: Radius.circular(40),
+        )),
+        backgroundColor: Colors.blue[300],
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+        title: Text('Flutter'),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite_outline, color: Colors.black),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.logout, color: Colors.black),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
