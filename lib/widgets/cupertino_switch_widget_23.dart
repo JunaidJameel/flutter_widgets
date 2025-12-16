d8cf754cbb98aa99f8f4031ffee85d9c99adf80f
Junaid Jameel <junaidjameel396@gmail.com>
2025-12-16 18:23:32 +0500

    widget

--- lib/widgets/cupertino_switch_widget_23.dart
+++ lib/widgets/cupertino_switch_widget_23.dart
@@ -10,17 +10,17 @@ class CupertinoSwitchWidget extends StatefulWidget {
 
 class _CupertinoSwitchWidgetState extends State<CupertinoSwitchWidget> {
   bool _isOn = false;
+
   @override
   Widget build(BuildContext context) {
     return CupertinoSwitch(
-      thumbColor: Colors.white,
-      // activeTrackColor: Colors.purple[200],
-      value: _isOn,
-      onChanged: (bool value) {
-        setState(() {
-          _isOn = value;
+        thumbColor: Colors.white,
+        activeTrackColor: Colors.purple[200],
+        value: _isOn,
+        onChanged: (bool status) {
+          setState(() {
+            _isOn = status;
+          });
         });
-      },
-    );
   }
 }
