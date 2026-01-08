83412dead9d37ea5ef6d2337230d5351cac509f5
Junaid Jameel <junaidjameel396@gmail.com>
2026-01-08 18:39:38 +0500

    widget

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/widgets/cached_network_image_widget_46.dart';
 import 'package:flutter_widgets/widgets/hero_animation_widget_43.dart';
 import 'package:flutter_widgets/widgets/material_button_widget_45.dart';
 import 'package:flutter_widgets/widgets/pageview_builder_widget_44.dart';
@@ -19,7 +20,7 @@ class HomeView extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              MaterialButtonWidget(),
+              CachedNetworkImageWidget(),
               Row(),
             ],
           ),
