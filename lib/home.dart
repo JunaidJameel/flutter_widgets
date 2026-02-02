093641aed9f0e710adb1c809c4f1ce466428c9e4
Junaid Jameel <junaidjameel396@gmail.com>
2026-02-02 20:57:34 +0500

    Widget

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_widgets/widgets/animate_do_package_70.dart';
 import 'package:flutter_widgets/widgets/aspect_ratio_widget_59.dart';
 import 'package:flutter_widgets/widgets/auto_complete_widget_60.dart';
+import 'package:flutter_widgets/widgets/card_swiper_package_71.dart';
 import 'package:flutter_widgets/widgets/drag_dropList_widget_61.dart';
 import 'package:flutter_widgets/widgets/grid_paper_widget_63.dart';
 import 'package:flutter_widgets/widgets/layout_builder_widget_64.dart';
@@ -19,11 +20,11 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade900,
+      backgroundColor: Colors.white.withValues(alpha: .8),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [AnimateDoPackage(), Row()],
+        children: [CardSwiperWidget(), Row()],
       ),
     );
   }
---
+++ lib/widgets/card_swiper_package_71.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
+
+class CardSwiperWidget extends StatelessWidget {
+  CardSwiperWidget({super.key});
+
+  final List<String> images = [
+    'https://i.pinimg.com/736x/3d/2e/b8/3d2eb880c177d0c3b688480fa8f788bf.jpg',
+    'https://i.pinimg.com/1200x/93/ea/7b/93ea7b33851e1b5cead891d86c74d7b2.jpg',
+    'https://i.pinimg.com/1200x/2f/b3/78/2fb3784b1ece91469a217a44aa5cfffe.jpg'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 550,
+      width: double.infinity,
+      child: CardSwiper(
+        backCardOffset: const Offset(0, 20),
+        cardBuilder: (context, index, horizontal, vertical) {
+          return ClipRRect(
+              borderRadius: BorderRadiusGeometry.circular(20),
+              child: Image.network(
+                images[index],
+                fit: BoxFit.cover,
+              ));
+        },
+        cardsCount: images.length,
+      ),
+    );
+  }
+}
