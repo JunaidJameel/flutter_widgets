f57975e7a86537dcb471c1b1cf1fa29dd2dd197f
Junaid Jameel <junaidjameel396@gmail.com>
2026-02-22 17:32:24 +0500

    widget

--- lib/widgets/shimmer_package_91.dart
+++ lib/widgets/shimmer_package_91.dart
@@ -1,32 +1,30 @@
-import 'package:flutter/material.dart';
 import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import 'package:flutter/material.dart';
 
 class AwesomeSnackbar extends StatelessWidget {
   const AwesomeSnackbar({super.key});
 
-  void _showSnackbar(BuildContext context) {
+  void _createSnackBar(BuildContext context) {
     final messenger = ScaffoldMessenger.of(context);
 
     messenger
       ..hideCurrentSnackBar()
-      ..showSnackBar(
-        SnackBar(
-          elevation: 0,
-          behavior: SnackBarBehavior.floating,
-          backgroundColor: Colors.transparent,
-          content: AwesomeSnackbarContent(
-            title: 'Success!',
-            message: 'Your action was completed successfully.',
-            contentType: ContentType.failure,
-          ),
+      ..showSnackBar(SnackBar(
+        elevation: 0,
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: Colors.transparent,
+        content: AwesomeSnackbarContent(
+          title: 'Sucess',
+          message: 'Your action was completed sucessfully.',
+          contentType: ContentType.warning,
         ),
-      );
+      ));
   }
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () => _showSnackbar(context),
+      onPressed: () => _createSnackBar(context),
       child: const Text('Show Snackbar'),
     );
   }
